e78e20d7bdcb1a30113c68dbca6b6e57981c30cd
kamrul-1985 <ikamrul113@gmail.com>
2023-10-21 17:56:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 5000
+ 
+const categories = require('./Data/categories.json');
+const cors = require('cors')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/categories',(req, res)=>{
+      res.send(categories);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
