1226eca72afbf804ecfa3ca0e180b91f8be08abe
kamrul-1985 <ikamrul113@gmail.com>
2023-11-12 20:05:43 +0600

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 7000;
  
 const categories = require('./Data/categories.json');
-const cors = require('cors')
+const news = require('./Data/news.json');
 
+const cors = require('cors');
 app.use(cors())
+
+app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).send('Something broke!');
+    });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
 app.get('/categories',(req, res)=>{
       res.send(categories);
 })
+
+app.get('/news',(req, res)=>{
+      res.send(news);
+})
+
+app.get('/news/:id', (req, res) =>{
+  const id = req.params.id
+  const selectedNews = news.find(n => n._id == id);
+  res.send(selectedNews);
+})
+
+app.get('/categories/:id', (req, res) =>{
+  const id = parseInt(req.params.id); 
+  // console.log(id);
+  if (id === 0){
+    res.send(news)
+  }
+  else{
+    const categoryNews = news.filter(n => n.category_id == id);
+    res.send(categoryNews);
+  }
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
